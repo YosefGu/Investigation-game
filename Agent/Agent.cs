4c529dd39477a82bb98485e497d939b27258ec5a
YosefGu <yos770yos@gmail.com>
2025-06-15 14:52:51 +0300

    Add abstract class for agent

---
+++ Agent/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation_game.Agent
+{
+    public abstract class Agent
+    {
+        public string Name { get; protected set; }
+        public string Rank { get; protected set; }
+        public bool Exposed { get; set; }
+        public Sensor[] Weaknesses { get; protected set; }
+        public Sensor[] AttachedSensors { get; set; }
+
+        public Agent(string name) 
+        {
+            this.Name = name;
+        }
+    }
+}
