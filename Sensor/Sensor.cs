0ebcab8eee90e53582502726d47d7851dff4db3d
YosefGu <yos770yos@gmail.com>
2025-06-15 15:29:58 +0300

    Add abstract class sensor

---
+++ Sensor/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation_game.Sensor
+{
+    public abstract class Sensor
+    {
+        public string Name { get; protected set; }
+
+        public abstract bool Activate(Sensor sensor);
+    }
+}
