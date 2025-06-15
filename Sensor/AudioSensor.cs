7416005ec4b9ba09bd737e6e6a01a7293f6d7065
YosefGu <yos770yos@gmail.com>
2025-06-15 15:30:29 +0300

    Add audio sensor class

---
+++ Sensor/AudioSensor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation_game.Sensor
+{
+    internal class AudioSensor : Sensor
+    {
+        public AudioSensor() 
+        {
+            this.Name = "Audio Sensor";
+        }
+
+        public override bool Activate(Sensor sensor)
+        {
+            return sensor is AudioSensor;
+        }
+    }
+}
