3b8f90d25d31452108358cdf00c82c0aea81c801
YosefGu <yos770yos@gmail.com>
2025-06-17 10:29:49 +0300

    Updating

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -3,29 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Investigation_game.Sensor;
 
 namespace Investigation_game.Agent
 {
     public abstract class Agent
     {
+        public enum SensorType {
+            AudioSensor,
+            ThermalSensos
+        }
         public string Name { get; protected set; }
         public int Rank { get; protected set; }
         public bool Exposed { get; set; }
-        public Sensor.Sensor[] WeaknessesSensor { get; protected set; }
-        public Sensor.Sensor[] AttachedSensor { get; set; }
+        public List<Sensor.Sensor> WeaknessesSensor { get; protected set; } = new List<Sensor.Sensor>();
+        public List<Sensor.Sensor> AttachedSensor { get; set; } = new List<Sensor.Sensor>();
 
         public Agent(string name) 
         {
             this.Name = name;
             this.Exposed = false;
-            this.WeaknessesSensor = new Sensor.Sensor[this.NumOfSlots()];
-            this.AttachedSensor = new Sensor.Sensor[this.NumOfSlots()];
-
+            GenerateWeaknessesSensor();
         }
 
-        protected int NumOfSlots()
+        private void GenerateWeaknessesSensor()
         {
-            return 2 * (Rank + 1);
+            int weaknesses = 2 * (Rank + 1);
+            Random rand = new Random();  
+            Array values = Enum.GetValues(typeof(SensorType));
+            for (int i = 0; i < weaknesses; i++) 
+            {
+                SensorType randomType = (SensorType)values.GetValue(rand.Next(values.Length));
+
+                switch (randomType) 
+                {
+                    case SensorType.AudioSensor:
+                        WeaknessesSensor.Add(new Sensor.AudioSensor());
+                        break;
+                    case SensorType.ThermalSensos:
+                        WeaknessesSensor.Add(new Sensor.ThermalSensor());
+                        break;
+                }
+            
+            }
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Investigation_game
     {
         static void Main(string[] args)
         {
+            Game.Game.StartGame();
         }
     }
 }
