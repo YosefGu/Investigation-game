1494d8d497138da896940bbccd7b85599d2b27e3
YosefGu <yos770yos@gmail.com>
2025-06-18 18:36:31 +0300

    Add factories to create sensor and agent

---
+++ Factories/SensorFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Investigation_game.Enums;
+using Investigation_game.Sensor;
+
+namespace Investigation_game.Factories
+{
+    using Sensor;
+
+    public class SensorFactory
+    {
+        private static Dictionary<Sensors, Func<Sensor>> sensorMap = new Dictionary<Sensors, Func<Sensor>>()
+        {
+            { Sensors.AudioSensor, () => new AudioSensor() },
+            { Sensors.ThermalSensor, () => new ThermalSensor() }
+        };
+
+        public static Sensor Create(Sensors sensorType)
+        {
+            if (sensorMap.ContainsKey(sensorType))
+                return sensorMap[sensorType]();
+            throw new ArgumentException("Unknown sensor type");
+        }
+    }
+}
